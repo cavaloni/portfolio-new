e39be5063afc92013c10b9ad28b2a00a94c83c5f
cavaloni <chad.avalon1@gmail.com>
2017-05-02 14:06:10 -0700

    scroll fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,12 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
     state = {
         navbarOpen: false,
     }
-    
-    var homeScroll = $('#informative').position().top - 71;
-    var aboutScroll = $('#about').position().top - 71;
-    var skillsScroll = $('#features').position().top - 71;
-    var projectsScroll = $('#docs').position().top - 71;
-    var whyChadScroll = $('#why-chad').position().top - 71;
-    var contactScroll = $('#contact').position().top - 71;
-
     var idNames = ['#homeScroll', '#aboutScroll', '#skillsScroll', '#projectsScroll', '#whyChadScroll', '#contactScroll']
+
     function rmvActive(except) {
-        idNames.forEach(function(name) {
+        idNames.forEach(function (name) {
             if (name === except) {
                 return
             }
@@ -21,51 +14,59 @@ jQuery(document).ready(function($) {
         })
     }
 
-    $(window).bind('scroll', function() {
-         var pos = $(window).scrollTop();
-         if (pos > 50) {
-             $('#header').addClass('top-navbar');
-         }
-         else {
-             $('#header').removeClass('top-navbar');
-         }
-         if (pos >= homeScroll && pos < aboutScroll) {
-             $('#homeScroll').addClass('active')
-             rmvActive('#homeScroll')
-         }
-         else if (pos >= aboutScroll && pos < skillsScroll) {
-             $('#aboutScroll').addClass('active')
-             rmvActive('#aboutScroll')
-         } else if (pos >= skillsScroll && pos < projectsScroll) {
-             $('#skillsScroll').addClass('active')
-             rmvActive('#skillsScroll')
-         } else if (pos >= projectsScroll && pos < whyChadScroll) {
-             $('#projectsScroll').addClass('active')
-             rmvActive('#projectsScroll')
-         } else if (pos >= whyChadScroll && pos < contactScroll) {
-             $('#whyChadScroll').addClass('active')
-             rmvActive('#whyChadScroll')
-         } else if (pos >= contactScroll) {
-             $('#contactScroll').addClass('active')
-             rmvActive('#contactScroll')
-         }
+    $(window).bind('scroll', function () {
+        var pos = $(window).scrollTop();
+        var homeScroll = $('#informative').position().top - 71;
+        var aboutScroll = $('#about').position().top - 71;
+        var skillsScroll = $('#features').position().top - 71;
+        var projectsScroll = $('#docs').position().top - 71;
+        var whyChadScroll = $('#why-chad').position().top - 71;
+        var contactScroll = $('#contact').position().top - 71;
+        if (pos > 50) {
+            $('#header').addClass('top-navbar');
+        } else {
+            $('#header').removeClass('top-navbar');
+        }
+        if (pos >= homeScroll && pos < aboutScroll) {
+            $('#homeScroll').addClass('active')
+            rmvActive('#homeScroll')
+        } else if (pos >= aboutScroll && pos < skillsScroll) {
+            $('#aboutScroll').addClass('active')
+            rmvActive('#aboutScroll')
+        } else if (pos >= skillsScroll && pos < projectsScroll) {
+            $('#skillsScroll').addClass('active')
+            rmvActive('#skillsScroll')
+        } else if (pos >= projectsScroll && pos < whyChadScroll) {
+            $('#projectsScroll').addClass('active')
+            rmvActive('#projectsScroll')
+        } else if (pos >= whyChadScroll && pos < contactScroll) {
+            $('#whyChadScroll').addClass('active')
+            rmvActive('#whyChadScroll')
+        } else if (pos >= contactScroll) {
+            $('#contactScroll').addClass('active')
+            rmvActive('#contactScroll')
+        }
     });
-   
+
     /* ======= ScrollTo ======= */
-    $('a.scrollto').on('click', function(e){
-        
+    $('a.scrollto').on('click', function (e) {
+
         //store hash
         var target = this.hash;
-                
+
         e.preventDefault();
-        
-		$('body').scrollTo(target, 800, {offset: -70, 'axis':'y', easing:'easeOutQuad'});
+
+        $('body').scrollTo(target, 800, {
+            offset: -70,
+            'axis': 'y',
+            easing: 'easeOutQuad'
+        });
         //close mobile menu after clicking
-		if ($('.navbar-close').hasClass('in')){
-			$('.navbar-close').removeClass('in').addClass('close');
-		}
-		
-	});
+        if ($('.navbar-close').hasClass('in')) {
+            $('.navbar-close').removeClass('in').addClass('close');
+        }
+
+    });
 
     $('.toggle-nav').on('click', function (e) {
         e.preventDefault();
